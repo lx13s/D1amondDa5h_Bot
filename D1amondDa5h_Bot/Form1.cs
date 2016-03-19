21ec809152c3ba06a1bc6a21297f29277735ebd4
tmp_online <savage@speccy.su>
2016-03-19 19:01:01 +0500

    First commit. Nothing interesting

---
+++ D1amondDa5h_Bot/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using mshtml;
+using Microsoft.VisualStudio.TestTools.UITesting;
+
+namespace D1amondDa5h_Bot
+{
+    public partial class Form1 : Form
+    {
+        Info info = new Info();
+        const int gameSizeWidth = 760;
+        const int gameSizeHeigth = 500;
+        const int rigthColWidth = 278;
+        //const int[] gameFrameSize = { 760, 500 };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            info.Visible = true;
+            info.AddLine("App started");
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // insert game position getter here.
+            IHTMLDocument2 doc2 = (IHTMLDocument2)webBrowser1.Document.DomDocument;
+            IHTMLElement2 elem2 = (IHTMLElement2)doc2.body;
+            int width = elem2.scrollWidth;
+            info.InfoText = elem2.scrollWidth.ToString();
+            int gamePosX = (width - gameSizeWidth - 278) / 2;
+            info.InfoText = "Game x = " + gamePosX.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            info.InfoText = "test";
+        }
+    }
+}
---
+++ D1amondDa5h_Bot/Info.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D1amondDa5h_Bot
+{
+    public partial class Info : Form
+    {
+        public Info()
+        {
+            InitializeComponent();
+        }
+
+        private void Info_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void AddLine(string line)
+        {
+            textBox1.Text += "\n" + line;
+        }
+
+        public string InfoText
+        {
+            get
+            {
+                return this.textBox1.Text;
+            }
+            set
+            {
+                this.textBox1.Text += Environment.NewLine + value;
+            }
+        }
+    }
+}
